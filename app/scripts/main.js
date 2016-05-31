957b0b5e5311032c96a737923fd751a69dff9a03
Marco Meluso <foriaa@gmail.com>
2016-05-31 16:23:17 +0200

    edit animation for flower

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,6 +2,6 @@ $(document).ready(function(){
     $('.start-animation').click(function(){
         console.log('hello');
         $(this).toggleClass('active');
-        $('#hexagon').toggleClass('flip');
+        $('#hexagon').toggleClass('sboccia');
     })
 })
